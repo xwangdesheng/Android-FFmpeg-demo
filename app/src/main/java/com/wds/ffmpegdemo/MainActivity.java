38edb7006e015ca86a3e472f68bac7763bfebb99
wangdesheng <wangdesheng2017@163.com>
2019-04-25 11:56:19 +0800

    .

--- app/src/main/java/com/wds/ffmpegdemo/MainActivity.java
+++ app/src/main/java/com/wds/ffmpegdemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
